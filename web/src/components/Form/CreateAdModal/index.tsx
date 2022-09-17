72bbf84e399e4e4735678bdedc5bcb2e0af39300
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-09-16 23:34:46 -0300

    add select with radix

---
+++ web/src/components/Form/CreateAdModal/index.tsx
@@ -0,0 +1,161 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import * as Checkbox from "@radix-ui/react-checkbox"
+import * as Select from "@radix-ui/react-select"
+import { Check, GameController } from "phosphor-react";
+import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
+import { Input } from "../Input";
+import { useEffect, useState } from "react";
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
+export function CreateAdModal() {
+  const [games, setGames] = useState<Game[]>([])
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => setGames(data))
+  }, [])
+  return (
+    <Dialog.Portal>
+      <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
+
+      <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
+        <Dialog.Title className='text-3xl font-black'>Publique um anúncio</Dialog.Title>
+
+        <form className="mt-8 flex flex-col gap-4">
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="game">Qual o game?</label>
+            <Select.Root>
+              <Select.Trigger className="flex justify-between bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500">
+                <Select.Value placeholder="Selecione o game que deseja jogar" />
+                <ChevronDownIcon />
+              </Select.Trigger>
+
+              <Select.Portal>
+                <Select.Content className="overflow-hidden bg-zinc-700 rounded text-white">
+                  <Select.ScrollUpButton />
+                  <Select.Viewport>
+                    {
+                      games.map(game => (
+                        <>
+                          <Select.Item key={game.id} value={game.title} className="p-2 text-sm hover:bg-violet-600">
+                            <Select.ItemText>{game.title}</Select.ItemText>
+                          </Select.Item>
+                        </>
+                      ))
+                    }
+                  </Select.Viewport>
+                </Select.Content>
+              </Select.Portal>
+            </Select.Root>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="name">Seu nome (ou nickname)</label>
+            <Input id="game" placeholder="Como te chamam dentro do game?" />
+          </div>
+
+          <div className="grid grid-cols-2 gap-6">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
+              <Input id='yearsPlaying' type='number' placeholder='Tudo bem ser ZERO' />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="discord">Qual seu Discord?</label>
+              <Input type="text" id="discord" placeholder="Usuario#0000" />
+            </div>
+          </div>
+
+          <div className="flex gap-6">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="weekDays">Quando costuma jogar?</label>
+              <div className="grid grid-cols-4 gap-2">
+                <button
+                  title="Domingo"
+                  className="w-8 h-8 rounded bg-zinc-900"
+                >
+                  D
+                </button>
+                <button
+                  title="Segunda"
+                  className="w-8 h-8 rounded bg-zinc-900"
+                >
+                  S
+                </button>
+                <button
+                  title="Terça"
+                  className="w-8 h-8 rounded bg-zinc-900"
+                >
+                  T
+                </button>
+                <button
+                  title="Quarta"
+                  className="w-8 h-8 rounded bg-zinc-900"
+                >
+                  Q
+                </button>
+                <button
+                  title="Quinta"
+                  className="w-8 h-8 rounded bg-zinc-900"
+                >
+                  Q
+                </button>
+                <button
+                  title="Sexta"
+                  className="w-8 h-8 rounded bg-zinc-900"
+                >
+                  S
+                </button>
+                <button
+                  title="Sábado"
+                  className="w-8 h-8 rounded bg-zinc-900"
+                >
+                  S
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2 flex-1">
+              <label htmlFor="hourStart">Qual horário do dia?</label>
+              <div className="grid grid-cols-2 gap-2">
+                <Input type="time" id="hourStart" placeholder="De" />
+                <Input type="time" id="hourEnd" placeholder="Até" />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-2 flex gap-2 text-sm">
+            <Checkbox.Root className="w-6 h-6 p-1 rounded bg-zinc-900">
+              <Checkbox.Indicator>
+                <Check className="w-4 h-4 text-emerald-400" />
+              </Checkbox.Indicator>
+            </Checkbox.Root>
+            Costumo me conectar ao chat de voz
+          </div>
+
+          <footer className="mt-4 flex justify-end gap-4">
+            <Dialog.Close
+              className="bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600"
+              type="button"
+            >
+              Cancelar
+            </Dialog.Close>
+            <button
+              type="submit"
+              className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600"
+            >
+              <GameController size={24} />
+              Encontrar Duo
+            </button>
+          </footer>
+        </form>
+      </Dialog.Content>
+    </Dialog.Portal>
+  )
+}
